e95807f4de104624a975bc2dcf67d82b0e62bfd7
Yogesh Srihari <yogesh.srihari@zefr.com>
2017-03-12 21:21:52 -0700

    Flask producer changes to handle bokeh server streaming plots and separation kafa producer

--- python/flask-producer.py
+++ python/flask-producer.py
@@ -1,10 +1,12 @@
+import csv
+import random
+import time
 
 import flask
 from flask import Flask, Response
-
 from flask import Flask, jsonify
 from jinja2 import Template
-import math
+
 
 from bokeh.plotting import figure
 from bokeh.models import AjaxDataSource
@@ -15,15 +17,17 @@ from bokeh.util.string import encode_utf8
 # Creating a flask app
 app = Flask(__name__)
 
-import csv
-
-import random
-import time
-import os
 from kafka import KafkaProducer
 
 
 def stream_template(template_name, **context):
+    """Utility function to test the flask
+    streaming capability
+
+    :param template_name: name of the template
+    :param context: context streaming .
+    :return:
+    """
     app.update_template_context(context)
     t = app.jinja_env.get_template(template_name)
     rv = t.stream(context)
@@ -54,10 +58,16 @@ train_Y_2 = []
 
 x, y = 0.0, 0.0
 
-script =''
-div =''
+script = ''
+div = ''
 
-def generate_next_row() :
+
+def generate_next_row():
+    """Utility for asynchronous request to
+    api for sending the data to kafka
+
+    Yields the one row from wine file at the time .
+    """
     dataset = read_input_file("./data_files/winequality-red-scaled.csv")
 
     for line in dataset:
@@ -67,111 +77,43 @@ def generate_next_row() :
             one_line.append(col)
 
         one_line = ','.join(str(i) for i in one_line)
-        all_values = one_line.split(',')
-        yield float(all_values[len(all_values) - 1])
 
+        r = random.randint(1, 10)  # random sleep time between [1,10] seconds
+        time.sleep(r)
 
-next_data_row = generate_next_row()
+        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+
+        producer.send('StreamAI', str.encode(one_line))
+        producer.flush()
+
+        yield one_line.join("Send this data to moron abo \n")
 
 
 @app.route("/data", methods=['POST'])
-def send_data_to_sgd():
+def send_data_to_bokeh_plot():
+    """ This sends to data to bokeh plot currently .
+    Usually there is a internal api called from an
+    exposed endpoint .
+    """
     global count
-    global train_Y_1
-    global train_Y_2
-    global script
-    global div
-
-    # producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    # producer.send(topic_name, str.encode(data))
-    # producer.flush()
-
-    from bokeh.plotting import figure, show, output_file
-    global x
-
-    # if len(train_Y_1) > 1:
-    #     x = train_Y_1[len(train_Y_1)-1]
-
     x = count
-    y = random.randint(1,10)
-    count = count +1
-    # y = x + random.randrange(0, 1)
-
-    print("Print X and Y : ")
-    print((x, y))
-
+    y = random.randint(1, 10)
+    count += 1
 
     return flask.jsonify(x=[x], y=[y])
 
-    # train_Y_1.append(float(all_values[len(all_values)-1]))
-    # train_Y_2.append(float(all_values[len(all_values)-1]))
-
-    # print("--------Debug Train Ys -----")
-    # print(train_Y_1)
-    # print(len(train_Y_1))
-    # print(train_Y_2)
-    # print(len(train_Y_2))
-    #
-    # count= count + 1
-    #
-    # colors = ['red' for x in range(len(train_Y_1))]
-    #
-    # p = figure(title="Flask Stack")
-    # p.xaxis.axis_label = 'Actual Value'
-    # p.yaxis.axis_label = 'Predicted Value'
-    #
-    # p.circle(train_Y_1, train_Y_2,
-    #          color=colors, fill_alpha=0.2, size=10)
-    #
-    # from bokeh.embed import components
-    # script, div = components(p)
-
 
 @app.route('/')
-def plotly():
-    """This reads the from winequality-red-scaled.csv for now .
+def send_data_to_kafka_consumer():
+        """This reads the from winequality-red-scaled.csv for now .
 
-    Returns actual html as a response red from the file
-    """
-
-    data_file_winequality = os.path.join(os.environ['STREAMAI_HOME'], 'python/data_files/winequality-red-scaled.csv')
-    dataset = read_input_file(data_file_winequality)
-
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-
-    for line in dataset:
-
-        one_line = []
-        for col in line:
-            one_line.append(col)
-
-        one_line = ','.join(str(i) for i in one_line)
+        """
+        return Response(generate_next_row(),mimetype="text/html")
 
 
-        # r = random.randint(1, 10)  # random sleep time between [1,10] seconds
-        # time.sleep(r)
-       # send_data_to_sgd("AboPunk", one_line)
-        all_values = one_line.split(',')
-        train_Y_1.append(float(all_values[len(all_values)-1]))
-        producer = KafkaProducer(bootstrap_servers='localhost:9092')
-        producer.send('AboPunk', str.encode(one_line))
-        producer.flush()
-
-    global train_Y_1
-    global train_Y_2
-    global script
-    global div
-
-    return Response(stream_template('log_lines.html', rows=len(train_Y_1)))
-
-
-# @app.route("/data", methods=['POST'])
-# def get_x():
-#     global x, y
-#     x = x + 0.1
-#     y = math.sin(x)
-#     return flask.jsonify(x=[x], y=[y])
-
+# Simple html / javascript template that could will be
+# replaced by bokek with embeded plot javascript
+# and css .
 template = Template('''<!DOCTYPE html>
 <html lang="en">
     <head>
@@ -188,60 +130,32 @@ template = Template('''<!DOCTYPE html>
 ''')
 
 
-@app.route('/second_shot')
-def second_shot():
+@app.route('/plot_demo')
+def bokeh_demo():
+    """End point demoing the real time injection of the data
+    to bokeh plots
 
-        streaming = True
-        source = AjaxDataSource(data_url="http://localhost:5000/data",
-                                polling_interval=3000, mode='append')
+    Returns streaming response from the bokeh server .
+    """
 
-        dataset = read_input_file("./data_files/winequality-red-scaled.csv")
+    source = AjaxDataSource(data_url="http://localhost:5000/data",
+                            polling_interval=3000, mode='append')
 
+    source.data = dict(x=[], y=[])
 
-        producer = KafkaProducer(bootstrap_servers='localhost:9092')
+    fig = figure(title="Streaming Example")
+    fig.line('x', 'y', source=source)
 
+    js_resources = INLINE.render_js()
+    css_resources = INLINE.render_css()
 
-        # for line in dataset:
-        #
-        #     one_line = []
-        #     for col in line:
-        #         one_line.append(col)
-        #
-        #     one_line = ','.join(str(i) for i in one_line)
-        #
-        #     # r = random.randint(1, 10)  # random sleep time between [1,10] seconds
-        #     # time.sleep(r)
-        #     global train_Y_1
-        #     global train_Y_2
-        #     # send_data_to_sgd("AboPunk", one_line)
-        #     all_values = one_line.split(',')
-        #     train_Y_1.append(float(all_values[len(all_values) - 1]))
-        #
-        #     # r = random.randint(1, 10)
-        #     # time.sleep(1)
-        #     # producer = KafkaProducer(bootstrap_servers='localhost:9092')
-        #     # producer.send('AboPunk', str.encode(one_line))
-        #     # producer.flush()
+    script, div = components(fig, INLINE)
 
+    html = template.render(
+        plot_script=script,
+        plot_div=div,
+        js_resources=js_resources,
+        css_resources=css_resources
+    )
 
-        source.data = dict(x=[], y=[])
-
-        fig = figure(title="Streaming Example")
-        fig.line('x', 'y', source=source)
-
-        js_resources = INLINE.render_js()
-        css_resources = INLINE.render_css()
-
-        script, div = components(fig, INLINE)
-
-        html = template.render(
-                plot_script=script,
-                plot_div=div,
-                js_resources=js_resources,
-                css_resources=css_resources
-            )
-
-        return encode_utf8(html)
-
-
-
+    return encode_utf8(html)
